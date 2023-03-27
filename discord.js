d345fb99c72738be44b54ccc6429258b9934d9ca
Tibo Pendino <tibo@starton.io>
2023-03-27 11:07:01 +0200

    feat(setup): release step1 of Epitech Workshop

---
+++ discord.js
@@ -0,0 +1,5 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+const BOT_TOKEN = process.env.BOT_TOKEN
+console.log(BOT_TOKEN)
---
+++ starton.js
@@ -0,0 +1,6 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+const STARTON_API_KEY = process.env.STARTON_API_KEY
+
+console.log(STARTON_API_KEY)
