6810a5362592bc1d3680b8585620690a55614bdb
Tibo Pendino <tibo@starton.io>
2023-03-29 08:49:09 +0200

    feat: add step5 correction

--- discord.js
+++ discord.js
@@ -1,5 +1,14 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 const dotenv = require('dotenv')
 dotenv.config()
 
 const BOT_TOKEN = process.env.BOT_TOKEN
 console.log(BOT_TOKEN)
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(BOT_TOKEN);
